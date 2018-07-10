93b8cd4ba0dabb3a02f9039d9ff7668cff041db9
wuboyou <wuboyou@WUBOYOU-PC>
2018-07-10 22:52:13 +0800

    添加项目文件。

---
+++ Hanoi/Hanoi.cpp
@@ -0,0 +1,24 @@
+// Hanoi.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+using namespace std;
+#include <iostream>
+#include <cstdio>
+
+void hannoi(int n, char from, char buffer, char to)
+{
+	if (n == 0) return;
+	hannoi(n - 1, from, to, buffer);
+	cout << "移动圆盘" << n << "，从 " << from << " 到 " << to << endl;
+	hannoi(n - 1, buffer, from, to);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	hannoi(n, 'A', 'B', 'C');
+    return 0;
+}
+
